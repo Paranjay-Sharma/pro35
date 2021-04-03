2154647080c5a7100cc33411fb15e864b47ec8e9
Paranjay Sharma <76864845+Paranjay-Sharma@users.noreply.github.com>
2021-04-03 18:47:13 +0530

    Add files via upload

---
+++ synchronousBallMovement-master/sketch.js
@@ -0,0 +1,31 @@
+var canvas;
+var playerCount;
+var database;
+var form,player,game;
+var gameState=0;
+  
+function setup(){
+canvas=createCanvas(400,400);
+database=firebase.database();
+}
+
+function draw(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
